1156ca9e9f974fd813f78fa43421de1ea90e7aa8
xingguo.lei <605088820@qq.com>
2020-04-13 14:32:16 +0800

    [add]修改网络库

--- app/src/main/java/com/xgl/mvp/MainActivity.java
+++ app/src/main/java/com/xgl/mvp/MainActivity.java
@@ -2,9 +2,15 @@ package com.xgl.mvp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.util.Log;
 
-import io.reactivex.schedulers.Schedulers;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
 
 /**
  * @author xingguolei
@@ -15,11 +21,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Schedulers.io();
-        findViewById(R.id.title_test).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            }
-        });
+        Observable.concat(testA(), testB())
+                .firstElement()
+                .toObservable()
+                .flatMap(new Function<Integer, ObservableSource<Integer>>() {
+                    @Override
+                    public ObservableSource<Integer> apply(Integer integer) throws Exception {
+                        Log.i("TAGS", "重试" + System.currentTimeMillis());
+                        return Observable.error(new Exception());
+                    }
+                })
+                .retryWhen(new Function<Observable<Throwable>, ObservableSource<?>>() {
+                    @Override
+                    public ObservableSource<?> apply(Observable<Throwable> throwableObservable) throws Exception {
+                        return Observable.timer(2, TimeUnit.SECONDS);
+                    }
+                })
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.i("TAGS", String.valueOf(integer));
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.i("TAGS", "onError");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.i("TAGS", "onComplete");
+                    }
+                });
+
+    }
+
+    private Observable<Integer> testA() {
+        if (System.currentTimeMillis() % 2 == 0) {
+            return Observable.empty();
+        } else {
+            return Observable.just(1);
+        }
+    }
+
+    private Observable<Integer> testB() {
+        return Observable.just(100);
     }
 }
--- libs/src/main/java/com/xgl/libs/network/ApiConnector.java
+++ libs/src/main/java/com/xgl/libs/network/ApiConnector.java
@@ -5,7 +5,7 @@ import java.util.concurrent.TimeUnit;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
@@ -33,7 +33,7 @@ public enum ApiConnector {
         mRetrofit = new Retrofit.Builder()
                 .baseUrl(baseUrl)
                 .client(okHttpClient)
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }
